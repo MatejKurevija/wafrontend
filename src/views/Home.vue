48de7f78d3bbffb5600ac18fa3c44e5b1b578a10
MatejKurevija <60134506+MatejKurevija@users.noreply.github.com>
2022-09-06 00:08:46 +0200

    added homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,45 @@
-<template></template>
+<template>
+  <div class="col-12">
+    <div
+      id="carouselExampleControls"
+      class="carousel slide"
+      data-ride="carousel"
+    >
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            class="d-block w-100"
+            src="https://www.apple.com/newsroom/images/product/iphone/standard/apple_iphone-12-spring21_lp_us_04202021.jpg.og.jpg?202208170813"
+            alt="First slide"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="card-header">Featured</div>
+  <div class="card text-center">
+    <div class="card-body">
+      <h5 class="card-title">Check out what you can buy!</h5>
+      <a href="Store" class="btn btn-primary">Store</a>
+    </div>
+  </div>
+  <div class="card text-center">
+    <div class="card-header">ABOUT US</div>
+    <div class="card-body">
+      <h5 class="card-title">Helping you find exotic equipment</h5>
+      <p class="card-text">
+        We help you buy items that are easy and hard to find!
+      </p>
+    </div>
+  </div>
+</template>
 
 <script>
 </script>
+
+<style scoped>
+.odmaknnivrh {
+  margin-top: 200px;
+}
+</style>
