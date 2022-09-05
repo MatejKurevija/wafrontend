c189a83c14943abc7941cf39fba1283cb7aabe65
MatejKurevija <60134506+MatejKurevija@users.noreply.github.com>
2022-09-06 00:11:42 +0200

    updated
    got products to be sent by id to shoping cart and placed images and cards

--- src/views/Store.vue
+++ src/views/Store.vue
@@ -2,39 +2,38 @@
   <div class="about">
     <div align="center">
       <h1>Welcome to the store!</h1>
-      <div>
-        <div v-for="proizvodi in proizvodi" v-bind:key="proizvodi.id">
-          <div class="nesto" style="width: 18rem">
-            <img class="card-img-top" alt="Card image cap" />
-            <div class="card-body">
-              <h5 class="card-title">Product: {{ proizvodi.Proizvod }}</h5>
-              <p class="card-text">
-                Model: <small>{{ proizvodi.ProizvodModel }}</small>
-              </p>
-              <p class="card-text">
-                Price: <small>{{ proizvodi.Cijena }}$</small>
-              </p>
 
-              <p class="card-text">
-                <small class="text-muted"
-                  >Description: {{ proizvodi.Opis }}</small
-                >
-              </p>
-              <p class="card-text">
-                <small class="text-muted"
-                  >Best use: {{ proizvodi.NajboljaUporaba }}</small
-                >
-              </p>
+      <div
+        class="nesto"
+        style="width: 18rem"
+        v-for="proizvodi in proizvodi"
+        v-bind:key="proizvodi.Proizvodi"
+      >
+        <img class="popravi" :src="proizvodi.imgURL" alt="Card image cap" />
+        <h5 class="card-title">Product: {{ proizvodi.Proizvod }}</h5>
+        <p class="card-text">
+          Model: <small>{{ proizvodi.ProizvodModel }}</small>
+        </p>
+        <p class="card-text">
+          Price: <small>{{ proizvodi.Cijena }}$</small>
+        </p>
 
-              <button
-                class="px-5 btn btn-sm btn-primary"
-                @click="AddToShopingCart()"
-              >
-                Add to cart
-              </button>
-            </div>
-          </div>
-        </div>
+        <p class="card-text">
+          <small class="text-muted">Description: {{ proizvodi.Opis }}</small>
+        </p>
+        <p class="card-text">
+          <small class="text-muted"
+            >Best use: {{ proizvodi.NajboljaUporaba }}</small
+          >
+        </p>
+
+        <button
+          class="px-5 btn btn-sm btn-primary"
+          @click="AddToShopingCart(proizvodi)"
+          :value="proizvodi.id"
+        >
+          Add to cart
+        </button>
       </div>
     </div>
   </div>
@@ -47,6 +46,7 @@ import { Proizvodi, ShopingCart, Auth } from "/service";
 export default {
   data() {
     return {
+      id: this.$route.params.id,
       proizvodi: {},
       Cart: {},
       auth: Auth.getUser().username,
@@ -63,18 +63,21 @@ export default {
     async pozovibackendproizvodi(term) {
       this.proizvodi = await Proizvodi.getAll(term);
     },
+
     async pozovibackendshopingcart(term) {
       this.shopingcart = await ShopingCart.getAll(term);
     },
-    async AddToShopingCart(odabraniProizvod) {
-            const proizvodZaDodavanje = this.proizvodi.find(
-        (proizvodi) => this.proizvodi === proizvodi.id
-      );
-      console.log(proizvodi.id);
-      this.Cart = {};
-      let newlist = await ShopingCart.add(this.Cart);
-      console.log("Spremljen u Shoping Cart", newlist.data);
-      this.feedback = "Added to Shoping cart";
+    async AddToShopingCart(pro) {
+      this.Cart = {
+        Auth: this.auth,
+        Proizvod: pro.Proizvod,
+        ProizvodModel: pro.ProizvodModel,
+        Cijena: pro.Cijena,
+        Opis: pro.Opis,
+        NajboljaUporaba: pro.NajboljaUporaba,
+      };
+      await ShopingCart.add(this.Cart);
+      alert("Added to Shoping cart");
     },
   },
 };
@@ -82,7 +85,6 @@ export default {
 
 <style >
 .nesto {
-  width: calc(50% - 100px);
   padding: 15px;
   text-align: center;
   font-size: 15px;
@@ -91,29 +93,16 @@ export default {
   border: 1px solid black;
   border-radius: 25px;
   margin-left: 25px;
-  margin-top: 25px;
+  float: left;
+  margin-bottom: 15px;
 }
-@media (min-width: 102px) {
-  .nesto {
-    float: left;
-  }
+.popravi {
+  margin-top: 10px;
+
+  height: 200px;
+  width: 200px;
 }
 </style>
      
 
 
-     this.proizvodi.forEach((pro) => {
-        this.Cart = {
-          Auth: this.auth,
-          Proizvod: pro.Proizvod,
-          ProizvodModel: pro.ProizvodModel,
-          Cijena: pro.Cijena,
-          Opis: pro.Opis,
-          NajboljaUporaba: pro.NajboljaUporaba,
-        };
-      });
-      if (this.Cart.Proizvod == this.Cart.id) {
-        let newlist = await ShopingCart.add(this.Cart);
-        console.log("Spremljen u Shoping Cart", newlist.data);
-        this.feedback = "Added to Shoping cart";
-      }
